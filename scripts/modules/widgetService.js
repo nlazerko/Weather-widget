6a53ab93615ec63692136b418a7b779e85087080
nlazerko <lazerkokolya@gmail.com>
2023-11-06 18:19:11 +0300

    добавил API

--- scripts/modules/widgetService.js
+++ scripts/modules/widgetService.js
@@ -1,15 +1,23 @@
+import { fetchWeather } from './APIservice.js';
 import {
   renderWidgetForecast,
   renderWidgetOther,
   renderWidgetToday,
 } from './render.js';
 
-export const startWidget = () => {
+export const startWidget = async () => {
   const widget = document.createElement('div');
   widget.classList.add('widget');
 
-  renderWidgetToday(widget);
-  renderWidgetOther(widget);
+  const dataWeather = await fetchWeather('Барановичи');
+
+  if (dataWeather.succes) {
+    renderWidgetToday(widget, dataWeather.data);
+    renderWidgetOther(widget, dataWeather.data);
+  } else {
+    showError();
+  }
+
   renderWidgetForecast(widget);
 
   return widget;
